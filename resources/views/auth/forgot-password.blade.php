23caf090d5ec8ebfe733167fa0d1092b8243a770
George W Neto <georgewneto@gmail.com>
2026-03-16 14:32:18 -0300

    fix: ajuste de imagens

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -16,7 +16,7 @@
                 <div class="card-body p-0">
                     <div class="row">
                         <div class="col-lg-6 d-none d-lg-flex">
-                            <div class="flex-grow-1 bg-password-image" style="background-image: url(&quot;assets/img/dogs/image1.jpeg&quot;);"></div>
+                            <div class="flex-grow-1 bg-password-image" style="background-image: url(&quot;assets/img/people/image1.png&quot;);"></div>
                         </div>
                         <div class="col-lg-6">
                             <div class="p-5">
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -251,7 +251,7 @@
                             <div class="nav-item dropdown no-arrow">
                                 <a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#">
                                     <span class="d-none d-lg-inline me-2 text-gray-600 small"><?php $user = session('user');echo $user['name']?></span>
-                                    <img class="border rounded-circle img-profile" src="{{url('assets/img/dogs/image2.jpeg')}}">
+                                    <img class="border rounded-circle img-profile" src="{{url('assets/img/people/image2.png')}}">
                                 </a>
                                 <div class="dropdown-menu shadow dropdown-menu-end animated--grow-in">
                                     <a class="dropdown-item" href="{{url('/profile')}}">
